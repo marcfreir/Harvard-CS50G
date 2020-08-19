7d61561436dc7affb92b2890785d34e48066e7fc
marcfreir <marcfreir@outlook.com>
2020-08-19 19:36:52 -0200

    Add directory sandbox with original file main.lua and resources with whaff-logo.png. Update main.lua

--- pongGame/main.lua
+++ pongGame/main.lua
@@ -1,31 +1,28 @@
 --[[
-    GD50 2018
-    Pong Remake
+    Game Dev 2020
 
-    pong-0
-    "The Day-0 Update"
+    Game name: Whaff (from "whiff-whaff")
+    Author   : Marc Freir
+    E-mail   : marcfreir@outlook.com
+    Version  : 0.1
+    Created with Lua and Löve2D in August 19, 2020
 
-    -- Main Program --
-
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
-
-    Originally programmed by Atari in 1972. Features two
-    paddles, controlled by players, with the goal of getting
-    the ball past your opponent's edge. First to 10 points wins.
-
-    This version is built to more closely resemble the NES than
-    the original Pong machines or the Atari 2600 in terms of
-    resolution, though in widescreen (16:9) so it looks nicer on 
-    modern systems.
+    >>Based on Pong by Colton Ogden (cogden@cs50.harvard.edu)<<
 ]]
 
+-- Screen size
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
---[[
-    Runs when the game first starts up, only once; used to initialize the game.
-]]
+-- Set window icon
+windowico = love.image.newImageData( "resources/whaff-logo.png" )
+success = love.window.setIcon( windowico )
+--print(success)
+
+-- Set window title
+love.window.setTitle( "Hey" )
+
+-- Initialize the game
 function love.load()
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
@@ -34,14 +31,13 @@ function love.load()
     })
 end
 
---[[
-    Called after update by LÖVE2D, used to draw anything to the screen, updated or otherwise.
-]]
+-- Draw on screen
 function love.draw()
     love.graphics.printf(
-        'Hello Pong!',          -- text to render
-        0,                      -- starting X (0 since we're going to center it based on width)
-        WINDOW_HEIGHT / 2 - 6,  -- starting Y (halfway down the screen)
-        WINDOW_WIDTH,           -- number of pixels to center within (the entire screen here)
-        'center')               -- alignment mode, can be 'center', 'left', or 'right'
+        'Hello World!',
+        0,
+        WINDOW_HEIGHT / 2 - 6,
+        WINDOW_WIDTH,
+        'center'
+    )
 end
